c2145e82e11972b6e7a4531895c78fdbf4bcab8d
Phạm Vũ Hùng <80585483+hungk64it1x@users.noreply.github.com>
2022-02-01 12:11:07 +0700

    Update train.py

--- train.py
+++ train.py
@@ -319,7 +319,7 @@ def train(hyp,  # path/to/hyp.yaml or hyp dictionary
             # Multi-scale
             if opt.multi_scale:
                 # sz = random.randrange(imgsz * 0.5, imgsz * 1.5 + gs) // gs * gs  # size
-                sz = np.random.choice([2240, 2560, 2880])
+                sz = np.random.choice([1280, 1600, 1920, 2240, 2560])
                 sf = sz / max(imgs.shape[2:])  # scale factor
                 if sf != 1:
                     ns = [math.ceil(x * sf / gs) * gs for x in imgs.shape[2:]]  # new shape (stretched to gs-multiple)
